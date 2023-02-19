9aaae96e78a617e221f4958c040ec6c2aa712eec
teemokoena <teemokoena@gmail.com>
2023-02-19 03:26:45 -0800

    task 5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i = i + 1)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
