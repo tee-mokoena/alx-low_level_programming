d14b97caf82ecd6d2578202c024866d096374c17
teemokoena <teemokoena@gmail.com>
2023-02-19 07:29:29 -0800

    task 6 numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/**
- * main - print single digits using only putchar.
+/** 
+ * main - print signle digits of base 10 using only putchar.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i = 0;
-
-	for (i >= 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i);
 		putchar('\n');
