7532aebd5d223bb123a96d61604846bd36152ef9
teemokoena <teemokoena@gmail.com>
2023-02-19 02:49:41 -0800

    task 3 alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 	int ch = 'a';
 	int CH = 'A';
 
-	while (ch <= 'a')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	while (CH <= 'A')
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
