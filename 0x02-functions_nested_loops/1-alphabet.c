b3cb1e6fd1b362791aaf034690a308d6dc740c24
teemokoena <teemokoena@gmail.com>
2023-02-22 10:39:37 -0800

    task 1 alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
 /**
- * main - print alphabet in lowercase
+ * main - entry point
+ * description - print alphabet in all lowercase
+ * Return: Always 0 (Success)
  */
-
 void print_alphabet(void);
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
+	return (0);
 }
