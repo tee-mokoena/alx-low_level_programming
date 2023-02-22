484d5c503a82ba317b87df3fcd3696622b2096b0
teemokoena <teemokoena@gmail.com>
2023-02-22 12:15:06 -0800

    tasl 3 islower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalphs - checks for aplhabetic characters
+ * Return: 1 if letter is lowercase, 0 if otherwise.
+ * @c: The character in ASCII code..
+ */
+int _isalphs(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
