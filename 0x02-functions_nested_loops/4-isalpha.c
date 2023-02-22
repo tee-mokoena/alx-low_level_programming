ae4b49f9df8d58122efe1496c752e664f05c2abb
teemokoena <teemokoena@gmail.com>
2023-02-22 12:31:07 -0800

    task 4 isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,13 +6,5 @@
  */
 int _isalphs(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
