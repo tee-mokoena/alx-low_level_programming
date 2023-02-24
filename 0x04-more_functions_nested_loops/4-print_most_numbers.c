2532fd2f320d5911c8d04f04977231f0ffd189e5
teemokoena <teemokoena@gmail.com>
2023-02-24 14:31:58 -0800

    task 4 print more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 - 9 exceept 2 and 4
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		while (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
