8cb908f821dd74f7116fc8ac766268e867c11e60
teemokoena <teemokoena@gmail.com>
2023-03-09 13:08:05 -0800

    task 7 palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string a palindrome
+ * @s: string input
+ * Return: 1 if is a palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recusrion - returns the length of a string
+ * @s: string input
+ * Return: length of the string
+ */
+int _strlen_recusrion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * check_pal - checks characters recursively for palindrome
+ * @s: string input
+ * @i: iterator
+ * @len: length of string
+ * Return: 1 if palindrom, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
