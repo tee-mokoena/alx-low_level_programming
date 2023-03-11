869d7dabb745cb683313def6403b22bbe6772b70
teemokoena <teemokoena@gmail.com>
2023-03-11 00:07:16 -0800

    task 6 prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,11 +11,11 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-		return (actual_prime(n, n - 1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
- * actual_prime - calculates if a number is prime recursively
+ * actual_prime - calculates if a number  is prime recursively
  * @n: input
  * @i: iterator
  * Return: 1 if prime, 0 if not
@@ -26,5 +26,5 @@ int actual_prime(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-		return (actual_prime(n, i - 1));
+	return (actual_prime(n, i - 1));
 }
