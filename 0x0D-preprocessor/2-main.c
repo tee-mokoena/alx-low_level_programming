1bd6c473cb918e34bc7f7b7284eda94ebff0755c
teemokoena <teemokoena@gmail.com>
2023-03-20 13:53:21 -0700

    task 2 print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the file followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
