53c676bd3c8390b37cc7a407b381f41990eaee36
teemokoena <teemokoena@gmail.com>
2023-03-23 13:33:40 -0700

    task 0 print name using pointers to function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
+#endif
