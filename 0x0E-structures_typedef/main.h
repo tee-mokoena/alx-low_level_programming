093b5c1747917403f14a756f3f373f505ab24ac8
teemokoena <teemokoena@gmail.com>
2023-03-25 12:16:34 -0700

    task 4 create a new dog

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -6,4 +6,5 @@ char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
